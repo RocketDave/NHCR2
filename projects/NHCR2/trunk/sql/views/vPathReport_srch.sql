15d4d280f127d283cb9a3131a4b23268e99e549a
judy <judy@e7168442-be37-0410-967b-9f791d7eb4bb>
2016-12-14 19:12:33 +0000

    initial views

---
+++ projects/NHCR2/trunk/sql/views/vPathReport_srch.sql
@@ -0,0 +1,10 @@
+﻿create view vPathReportSrch as select
+    last_name,
+    first_name,
+    middle_name
+    to_char(date_of_birth,'yyyy-mm-dd') as date_of_birth,
+    facility_id,
+    medical_record_number,
+    from event e join person p on e.person_id = p.person_id 
+
+GRANT select on vPathReportSrch to nhcr2_rc;

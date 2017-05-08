fb6b6886972683fe39f0de48cb6243f7a278bb13
judy <judy@e7168442-be37-0410-967b-9f791d7eb4bb>
2017-05-08 19:44:35 +0000

    new procedures

---
+++ sql/procsfuncs/check_barcode.sql
@@ -0,0 +1,28 @@
+create or replace function check_barcode (in_barcode varchar, in_facility_id varchar)
+returns integer as
+$BODY$
+declare
+    lcl_letter varchar;
+    lcl_num integer;
+    lcl_count integer;
+
+begin
+    select substring (in_barcode,1,1) into lcl_letter;
+    select safe_cast(substring (in_barcode,2,7),null::integer) into lcl_num;
+
+    select count(*) from form_log where facility_id = in_facility_id  and 
+        substring(start_barcode,1,1) = lcl_letter  and 
+        lcl_num >= safe_cast(substring(start_barcode,2,7),null::integer) and 
+        substring(end_barcode,1,1) = lcl_letter  and 
+        lcl_num <= safe_cast(substring(end_barcode,2,7),null::integer)
+        into lcl_count;
+
+    if lcl_count > 0 then
+        return 1;
+    else
+        return 0;
+    end if;
+end;
+$BODY$
+language plpgsql;
+    

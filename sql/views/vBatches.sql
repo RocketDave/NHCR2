00a52e4d7b6295a1d15376a7fb6ed1676097c625
judy <judy@e7168442-be37-0410-967b-9f791d7eb4bb>
2017-12-15 16:02:46 +0000

    added fields

--- sql/views/vBatches.sql
+++ sql/views/vBatches.sql
@@ -9,6 +9,15 @@
     to_char(arrival_date,'yyyy-mm-dd') as arrival_date,
     entry_completed,
     to_char(entry_completed_on,'yyyy-mm-dd') as entry_completed_on,
+    refusals_with_r ,
+    refusals_without_r ,
+    refusals_without_g ,
+    unsigned_with_r ,
+    unsigned_without_r ,
+    orphans,
+    language,
+    disabled,
+    not_approached,
     b.comments
     from batch b join facility f on b.facility_id = f.facility_id 
     order by b.batch_id;

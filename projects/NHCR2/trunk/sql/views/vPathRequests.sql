3002aa677e326386f944f1e63ce3e94eaeac16de
judy <judy@e7168442-be37-0410-967b-9f791d7eb4bb>
2018-01-15 20:26:02 +0000

    changed fields

--- projects/NHCR2/trunk/sql/views/vPathRequests.sql
+++ projects/NHCR2/trunk/sql/views/vPathRequests.sql
@@ -1,7 +1,7 @@
 ﻿create view vpathrequests as select 
     path_request.path_request_id, path_request.event_id,
-    to_char(path_request.print_date::timestamp with time zone, 'yyyy-mm-dd'::text) AS print_date, 
-    to_char(path_request.recvd_date::timestamp with time zone, 'yyyy-mm-dd'::text) AS recvd_date
+    print_date, 
+    recvd_date
     from path_request;
 
 GRANT ALL ON TABLE vpathrequests TO nhcr2_rc;

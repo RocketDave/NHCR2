97113ffb791fbc71f07848d4b12dca4b3d9ff8fa
judy <judy@e7168442-be37-0410-967b-9f791d7eb4bb>
2018-01-15 20:24:57 +0000

    new views

---
+++ sql/views/vColo_p.sql
@@ -0,0 +1,9 @@
+﻿create view vcolo_p as select
+    colo_id,
+    event_id,
+    facility_id,
+    exam_date,
+    crs_batch,
+    scan_batch
+    from colo;
+    grant select on vcolo_p to NHCR2_rc;
---
+++ sql/views/vpatheport_srch.sql
@@ -0,0 +1,19 @@
+﻿create view vpathreportsrch as select
+    p.person_id, 
+    p.refused,
+    event_date, 
+    e.event_id, 
+    e.event_type,
+    e.medical_record_number,
+    e.endo_code,
+    p.last_name, 
+    p.first_name, 
+    p.middle_name, 
+    dob ,
+    p.gender_calcd, 
+    b.facility_id,
+    facility_name
+    from Event e join  Person p on e.person_id = p.person_id 
+    left outer join  Batch b on e.batch_id = b.batch_id 
+    left outer join facility f on b.facility_id = f.facility_id;
+grant select on vpathreportsrch to NHCR2_rc;

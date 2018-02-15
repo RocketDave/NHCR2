a3742c145e460191905a45ee87b64b9717acb336
judy <judy@e7168442-be37-0410-967b-9f791d7eb4bb>
2018-02-15 15:14:38 +0000

    added calculation for fu_form_completed

--- sql/procsfuncs/set_calculated_variables2.sql
+++ sql/procsfuncs/set_calculated_variables2.sql
@@ -8,6 +8,7 @@ begin
     update colo set find_calc_cancer = get_find_calc_cancer (in_colo_id) where colo_id = in_colo_id;
     update colo set find_calc_other = get_find_calc_other (in_colo_id) where colo_id = in_colo_id;
     update colo set find_calc_normal = get_find_calc_normal (in_colo_id) where colo_id = in_colo_id;
+    update colo set fu_form_completed = get_fu_form_completed (in_colo_id) where colo_id = in_colo_id;
 
 end;
 $BODY$

24f6ed4abf3b3a1f96e11aaf507ff66ab9ac48fc
judy <judy@e7168442-be37-0410-967b-9f791d7eb4bb>
2018-05-10 20:44:17 +0000

    changed error messages

--- sql/procsfuncs/get_related_eventid.sql
+++ sql/procsfuncs/get_related_eventid.sql
@@ -11,7 +11,10 @@ begin
     if exists (select * from specimen s join path_report p on s.path_report_id = p.path_report_id where p.event_id = in_event_id) then lcl_related = lcl_related || 'specimen, '; end if;
     if exists (select * from path_request where event_id = in_event_id) then lcl_related = lcl_related || 'path_request, '; end if;
     if exists (select * from polyp2 where event_id = in_event_id) then lcl_related = lcl_related || 'polyp2, '; end if;
-    if exists (select * from survey where event_id = in_event_id) then lcl_related = lcl_related || 'survey, '; end if;
+    if exists (select * from survey where event_id = in_event_id) then lcl_related = lcl_related || 'survey '; end if;
+    if (substring(lcl_related,length(lcl_related),1) = ',') then
+        lcl_related = substring(lcl_related,1,length(lcl_related)-1);
+    end if;
 
     return lcl_related;
 

d1594d33dd21a7831c9454bedd223bf2d268aa10
judy <judy@e7168442-be37-0410-967b-9f791d7eb4bb>
2018-06-08 20:05:58 +0000

    new tables

---
+++ sql/tables/dup_events_tbl.sql
@@ -0,0 +1,12 @@
+﻿create table dup_events (
+    event_id serial not null,
+    person_id integer,
+    event_date date,
+    patient_barcode varchar,
+    endo_barcode varchar,
+    event_id2 serial not null,
+    person_id2 integer,
+    event_date2 date,
+    patient_barcode2 varchar,
+    endo_barcode2 varchar)
+)

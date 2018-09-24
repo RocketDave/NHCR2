14adde6f79b32f4946ce3abc73a0660634cf30e6
judy <judy@e7168442-be37-0410-967b-9f791d7eb4bb>
2018-09-24 20:14:38 +0000

    new web pages

---
+++ phproot/getData5.php
@@ -0,0 +1,13 @@
+<?php 
+
+// This is just an example of reading server side data and sending it to the client.
+// It reads a json formatted text file and outputs it.
+$path =getcwd();
+$filename = $path."/data/MyData5.json";
+
+$string = file_get_contents($filename);
+echo $string;
+
+// Instead you can query your database and parse into JSON etc etc
+
+?>

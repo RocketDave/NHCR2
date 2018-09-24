f706944ba37faba0da3cfa6673432169d69d59fb
judy <judy@e7168442-be37-0410-967b-9f791d7eb4bb>
2018-09-24 20:35:30 +0000

    initial changes

--- projects/NHCR2/trunk/sql/procsfuncs/get_duplicate_events.sql
+++ projects/NHCR2/trunk/sql/procsfuncs/get_duplicate_events.sql
@@ -1,3 +1,3 @@
-select * from (select event_id,person_id,event_date,endo_barcode,patient_barcode from event where patient_barcode is null order by person_id) as no_patient join
-(select event_id,person_id,event_date,endo_barcode,patient_barcode from event where endo_barcode is null order by person_id) as no_endo 
-on no_patient.person_id = no_endo.person_id and  no_patient.event_date = no_endo.event_date 
+insert into dup_events select * from (select event_id,person_id,event_date,patient_barcode,endo_barcode from event where patient_barcode is null order by person_id) as no_patient join
+(select event_id as event_id2,person_id as person_id2,event_date as event_date2,patient_barcode as patient_barcode2,endo_barcode as endo_barcode2 from event where endo_barcode is null order by person_id2) as no_endo 
+on no_patient.person_id = no_endo.person_id2 and  no_patient.event_date = no_endo.event_date2 

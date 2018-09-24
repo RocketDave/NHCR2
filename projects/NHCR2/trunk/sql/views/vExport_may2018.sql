193f1eec0b05f9fb8ce902fb030b4476428dda0a
judy <judy@e7168442-be37-0410-967b-9f791d7eb4bb>
2018-09-24 20:35:09 +0000

    changed to materialized view

--- projects/NHCR2/trunk/sql/views/vExport_may2018.sql
+++ projects/NHCR2/trunk/sql/views/vExport_may2018.sql
@@ -1,2 +1,2 @@
 --need this because Stat transfer does not see materialized viuews
-create view mvExport_may2018_e as select * from mvExport_may2018_e;
+create view vExport_may2018_e as select * from mvExport_may2018_e;

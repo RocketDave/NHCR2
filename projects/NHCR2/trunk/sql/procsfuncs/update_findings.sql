049aa2a26538083812597f571379c81a7405e29b
judy <judy@e7168442-be37-0410-967b-9f791d7eb4bb>
2018-09-24 20:37:00 +0000

    added initialization, added return message

--- projects/NHCR2/trunk/sql/procsfuncs/update_findings.sql
+++ projects/NHCR2/trunk/sql/procsfuncs/update_findings.sql
@@ -1,7 +1,10 @@
-create function update_findings()
-returns void as 
+create or replace function public.update_findings()
+returns table (
+    lcl_message varchar) AS
 $BODY$
 begin
+
+perform initialize_findings();
 
 update findings set proximal_serrated = get_proximal_serrated(event_id);
 
@@ -17,6 +20,11 @@ update findings set diagnostic = 1 where indication_calculated = 'Diagnostic';
 
 update findings set eligible = get_eligible(colo_id);
 
+
+lcl_message = 'FINDINGS table has been updated';
+return query select lcl_message;
 end;
 $BODY$
 language plpgsql;
+
+GRANT EXECUTE ON FUNCTION public.update_findings() TO nhcr2_rc;
